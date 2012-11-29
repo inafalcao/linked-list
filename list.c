42172b13373972962101f443426c612139cb57b5
inafalcao <inafalcao@gmail.com>
2012-11-29 17:02:14 -0300

    Defining structure.

--- list.c
+++ list.c
@@ -1,1 +1,74 @@
-#include "list.h"
+//#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int info;
+	struct node *prox;
+};
+
+
+typedef struct node Node;
+
+void printList(Node *list);
+Node *findElement(Node *list, int element);
+int isEmpty(Node *list);
+void insert(Node *list, int element);
+
+/*
+* Prints the list.
+*
+*/
+void printList(Node *list) {
+	Node *p;
+	for (p = list->prox; p != NULL; p = p->prox) {
+		printf(" %d -", p->info);
+	}
+	printf("\n");
+}
+
+/*
+*
+*
+*/
+Node *findElement(Node *list, int element) {
+	Node *p = list;
+	while(p->info != element) {
+		p = p->prox;
+	}
+	return p;
+}
+
+/*
+*
+*
+*/
+int isEmpty(Node *list) {
+	return (list == NULL);
+}
+
+void insert(Node *list, int element) {
+	Node *newNode;
+	newNode = (Node*) malloc(sizeof(Node));
+	newNode->info = element;
+	newNode->prox = list->prox;
+	list->prox = newNode;	
+}
+
+int main(int argc, char const *argv[])
+{
+	Node *list;
+	list->prox = NULL;
+	int element1 = 1;
+	int element2 = 2;
+	printf(NULL);
+
+	printList(list);
+	insert(list, element1);
+	insert(list, element2);
+
+	printList(list);
+
+	//return 0;
+}
+
--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 
+/*
+*
+* Linked list library usage.
+* @author Inaciane Monteiro
+*
+*/
 
 #include <stdio.h>
+#include "list.h"
 
+
+struct  node {
+	int info;
+	struct node *prox;
+};
+
+typedef struct node Node;
 
 
 int int main(int argc, char const *argv[])
